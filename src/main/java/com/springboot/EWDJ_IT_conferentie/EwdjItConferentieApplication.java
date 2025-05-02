fe74b064a3bf02a9411164e43cfa67d6cf737a98
Dylan De Man <dylan.deman@student.hogent.be>
2025-05-02 15:18:28 +0200

    project created

---
+++ src/main/java/com/springboot/EWDJ_IT_conferentie/EwdjItConferentieApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.EWDJ_IT_conferentie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwdjItConferentieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwdjItConferentieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/EWDJ_IT_conferentie/EwdjItConferentieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.EWDJ_IT_conferentie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwdjItConferentieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
